de7ade5dc540de870890b5e95e4a60dbe36f1e11
sigmundhh <sigmundhh@gmail.com>
2019-06-21 14:53:05 +0200

    Create tho node in main(). Rename header to .hpp. Start the publisher implementation.

---
+++ include/path_runner.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#pragma once
+
+#include "ros/ros.h"
+#include "std_srvs/Trigger.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "std_srvs/Empty.h"
+#include <geometry_msgs/Twist.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+#include <stdlib.h>
+#include <tf/transform_datatypes.h>
+
+using namespace std;
+
+
+
+
+
